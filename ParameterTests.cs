7508b899982bd23d9472bde6e66a3633a0665478
Shmueli Englard <shmueli.yosef@englard.net>
2016-05-11 22:37:27 -0400

    Added parameter tests class

---
+++ ParameterTests.cs
@@ -0,0 +1,29 @@
+﻿namespace System
+{
+    internal static class ParameterTests
+    {
+        public static void NotNull<T>(this T @this, string paramName) where T : class
+        {
+            if (ReferenceEquals(@this, null))
+            {
+                throw new ArgumentNullException(paramName, ErrorStrings.CannotBeNull(paramName));
+            }
+        }
+
+        public static void NotNullOrEmpty(this string @this, string paramName)
+        {
+            if (string.IsNullOrEmpty(@this))
+            {
+                throw new ArgumentNullException(paramName, ErrorStrings.CannotBeNull(paramName));
+            }
+        }
+
+        public static void NotNullOrWhiteSpace(this string @this, string paramName)
+        {
+            if (string.IsNullOrWhiteSpace(@this))
+            {
+                throw new ArgumentNullException(paramName, ErrorStrings.CannotBeNull(paramName));
+            }
+        }
+    }
+}
