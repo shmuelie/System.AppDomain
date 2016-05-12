425e8adab946a92ead8df618c388a8fc5cfa3bc7
Shmueli Englard <shmueli.yosef@englard.net>
2016-05-11 22:31:15 -0400

    Added error string generator

---
+++ ErrorStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace System
+{
+    public static class ErrorStrings
+    {
+        public static string CannotBeNull(string paramName)
+        {
+            return $"'{paramName}' cannot be null (Nothing in Visual Basic)";
+        }
+        public static string CannotBeNullOrEmpty(string paramName)
+        {
+            return $"'{paramName}' cannot be empty or null (Nothing in Visual Basic)";
+        }
+
+        public static string CannotBeNullOrWhitespace(string paramName)
+        {
+            return $"'{paramName}' cannot be empty, only whitespace, or null (Nothing in Visual Basic)";
+        }
+    }
+}
