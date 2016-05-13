0f3984e57744178977c091b886bc73dff0f389dc
Shmueli Englard <shmueli.yosef@englard.net>
2016-05-13 00:47:18 -0400

    Added documentation for public methods Based on MSDN documentation

--- AppDomain.cs
+++ AppDomain.cs
@@ -4,6 +4,13 @@ using System.Reflection;
 
 namespace System
 {
+    /// <summary>
+    ///     Represents an application domain, which is an isolated environment where applications execute. This class cannot be inherited.
+    /// </summary>
+    /// <remarks>
+    ///     The AppDomain class implements a set of events that enable applications to respond when an assembly is loaded.
+    /// </remarks>
+    /// <threadsafety static="true" instance="false"/>
     public sealed class AppDomain
     {
         internal static readonly Type RealType = typeof(string).GetTypeInfo().Assembly.GetType(typeof(AppDomain).FullName);
@@ -41,6 +48,12 @@ namespace System
             return new AppDomain(RealType.GetProperty(nameof(CurrentDomain), BindingFlags.DeclaredOnly | BindingFlags.Static | BindingFlags.Public).GetValue(null));
         }
 
+        /// <summary>
+        ///     Gets the current application domain for the current application.
+        /// </summary>
+        /// <value>
+        ///     The current application domain.
+        /// </value>
         public static AppDomain CurrentDomain
         {
             get
@@ -70,6 +83,12 @@ namespace System
             resourceResolveReal = CreateResolveEventHandler(nameof(OnResourceResolve));
         }
 
+        /// <summary>
+        ///     Gets the base directory that the assembly resolver uses to probe for assemblies.
+        /// </summary>
+        /// <value>
+        ///     The base directory that the assembly resolver uses to probe for assemblies.
+        /// </value>
         public string BaseDirectory
         {
             get
@@ -127,6 +146,18 @@ namespace System
             return assemblyResolve?.Invoke(this, args);
         }
 
+        /// <summary>
+        ///     Occurs when the resolution of an assembly fails.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         It is the responsibility of the <see cref="ResolveEventHandler"/> for this event to return the assembly that is specified by the <see cref="ResolveEventArgs.Name"/> property, or to return <see langword="null"/> if the assembly is not recognized.
+        ///     </para>
+        ///     <para>
+        ///         For guidance on the use of this event, see <see href="https://msdn.microsoft.com/en-us/library/ff527268(v=vs.110).aspx">Resolving Assembly Loads</see>.
+        ///     </para>
+        /// </remarks>
+        /// <seealso cref="ResolveEventArgs.RequestingAssembly"/>
         public event ResolveEventHandler AssemblyResolve
         {
             add
@@ -146,6 +177,21 @@ namespace System
             return typeResolve?.Invoke(this, args);
         }
 
+        /// <summary>
+        ///     Occurs when the resolution of a type fails.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The TypeResolve event occurs when the common language runtime is unable to determine the assembly that can create the requested type. This can occur if the type is defined in a dynamic assembly, or the type is not defined in a dynamic assembly but the runtime does not know which assembly the type is defined in. The latter situation can occur when <see cref="Type.GetType(string)"/> is called with a type name that is not qualified with the assembly name.
+        ///     </para>
+        ///     <para>
+        ///         The <see cref="ResolveEventHandler"/> for this event can attempt to locate and create the type.
+        ///     </para>
+        ///     <para>
+        ///         However, the TypeResolve event does not occur if the runtime knows it is not possible to find a type in certain assemblies. For example, this event does not occur if the type is not found in a static assembly because the runtime knows types cannot be added dynamically to static assemblies.
+        ///     </para>
+        /// </remarks>
+        /// <seealso cref="ResolveEventArgs.RequestingAssembly"/>
         public event ResolveEventHandler TypeResolve
         {
             add
@@ -165,6 +211,18 @@ namespace System
             return resourceResolve?.Invoke(this, args);
         }
 
+        /// <summary>
+        ///     Occurs when the resolution of a resource fails because the resource is not a valid linked or embedded resource in the assembly.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         The <see cref="ResolveEventHandler"/> for this event can attempt to locate the assembly containing the resource and return it.
+        ///     </para>
+        ///     <note type="important">
+        ///         This event is not raised if resolution fails because no file can be found for a valid linked resource. It is raised if a manifest resource stream cannot be found, but it is not raised if an individual resource key cannot be found.
+        ///     </note>
+        /// </remarks>
+        /// <seealso cref="ResolveEventArgs.RequestingAssembly"/>
         public event ResolveEventHandler ResourceResolve
         {
             add
