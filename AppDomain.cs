d301c8d3986eb3d472d8adbb207d154fe9fcca3c
Shmueli Englard <shmueli.yosef@englard.net>
2016-05-13 00:22:18 -0400

    Removed unneeded usings

--- AppDomain.cs
+++ AppDomain.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Reflection;
+﻿using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace System
 {
