7205ae6e62d917042979141b542c267f20d18f30
Shmueli Englard <shmueli.yosef@englard.net>
2016-05-17 11:38:40 -0400

    Simplified null errors

--- ErrorStrings.cs
+++ ErrorStrings.cs
@@ -2,18 +2,20 @@
 {
     internal static class ErrorStrings
     {
+        private const string NULL = "null (Nothing in Visual Basic)";
+
         public static string CannotBeNull(string paramName)
         {
-            return $"'{paramName}' cannot be null (Nothing in Visual Basic)";
+            return $"'{paramName}' cannot be {NULL}";
         }
         public static string CannotBeNullOrEmpty(string paramName)
         {
-            return $"'{paramName}' cannot be empty or null (Nothing in Visual Basic)";
+            return $"'{paramName}' cannot be empty or {NULL}";
         }
 
         public static string CannotBeNullOrWhitespace(string paramName)
         {
-            return $"'{paramName}' cannot be empty, only whitespace, or null (Nothing in Visual Basic)";
+            return $"'{paramName}' cannot be empty, only whitespace, or {NULL}";
         }
     }
 }
