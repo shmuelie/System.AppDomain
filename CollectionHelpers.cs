d66a56b450f9a500cceb595bc16ccc0a13a3c234
Shmueli Englard <shmueli.yosef@englard.net>
2016-07-18 15:56:09 -0400

    Added new way to create collections

---
+++ CollectionHelpers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace System
+{
+    internal static class CollectionHelpers
+    {
+        public static IEnumerable<T> Collect<T>(this T @this)
+        {
+            yield return @this;
+        }
+
+        public static IEnumerable<T> AndThis<T>(this IEnumerable<T> @this, T also)
+        {
+#pragma warning disable HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator
+            foreach (T t in @this)
+#pragma warning restore HeapAnalyzerEnumeratorAllocationRule // Possible allocation of reference type enumerator
+            {
+                yield return t;
+            }
+            yield return also;
+        }
+    }
+}
